cca54d092d7b9bf8f5bc8143175f8b32976669d2
olamidotunIY <olamidotun225@gmail.com>
2025-08-31 15:10:09 +0100

    Prepare for railway

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -3,15 +3,23 @@ from django.shortcuts import render
 from visits.models import PageVisit
 
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
     path = request.path
     qs = PageVisit.objects.all()
     page_qs = qs.filter(path=path)
     PageVisit.objects.create(path=path)
+    try:
+        percent = (page_qs.count() * 100) / qs.count()
+    except:
+        percent = 0
     my_context = {
         "page_visit_count": page_qs.count(),
         "total_visit_count": qs.count(),
-        "percent": (page_qs.count() * 100) / qs.count(),
+        "percent": percent,
     }
 
     return render(request, "home.html", my_context)
