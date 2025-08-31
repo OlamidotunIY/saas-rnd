7b217446a15f6ec21ade7b878ace4188c136dbfb
olamidotunIY <olamidotun225@gmail.com>
2025-08-31 16:14:00 +0100

    build(deps): update requirements.txt and settings.py

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fxv1$*_zpgkw=4*-!94ei&-c+h-+mu4@d&_c*vmwa$hax6y7nf"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+
+print("DEBUG:", DEBUG)
 
 ALLOWED_HOSTS = [".railway.app"]
 
